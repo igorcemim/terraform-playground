b97cdfc64c31e374bdae5ec65d94a1d87fc912bc
Igor Cemim <igor@cemim.com.br>
2020-04-14 01:25:17 -0300

    EC2 com dependência no S3 e regra para saída no security group

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "aws_vpc" "vcp_igor" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-
   tags = {
     Name = "vcp_igor_1"
   }
@@ -47,22 +46,23 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.public-ap-1a.id
+  subnet_id = aws_subnet.subnet_public_1a.id
 }
 
 # Subnet
-resource "aws_subnet" "public-ap-1a" {
+resource "aws_subnet" "subnet_public_1a" {
   vpc_id = aws_vpc.vcp_igor.id
   cidr_block = "10.0.100.0/24"
 }
 
-resource "aws_route_table_association" "public-ap-1a" {
-  subnet_id = aws_subnet.public-ap-1a.id
+resource "aws_route_table_association" "subnet_public_1a" {
+  subnet_id = aws_subnet.subnet_public_1a.id
   route_table_id = aws_route_table.igw.id
 }
 
+# Security
 resource "aws_security_group" "security_group_ssh" {
-    name = "acesso-ssh"
+    name = "servidor-ssh"
     ingress {
         from_port = 22
         to_port = 22
@@ -71,6 +71,43 @@ resource "aws_security_group" "security_group_ssh" {
     }
     vpc_id = aws_vpc.vcp_igor.id
 }
+resource "aws_security_group" "security_group_http" {
+  name = "servidor-http"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = aws_vpc.vcp_igor.id
+}
+# Permite qualquer tráfego de saída
+resource "aws_security_group" "security_group_exit_enable_all" {
+  name = "exit-enable-all"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = aws_vpc.vcp_igor.id
+}
+
+# S3
+resource "aws_s3_bucket" "igor_dev4_s3" {
+  bucket = "igor-dev4-s3"
+  acl    = "private"
+
+  tags = {
+    Name = "igor-dev4-s3"
+  }
+}
 
 # EC2
 resource "aws_instance" "igor_dev" {
@@ -78,10 +115,30 @@ resource "aws_instance" "igor_dev" {
     ami = "ami-07ebfd5b3428b6f4d" # Ubuntu Server 18.04 LTS
     instance_type = "t2.micro"
     key_name = "terraform-aws"
-    subnet_id = aws_subnet.public-ap-1a.id
-    vpc_security_group_ids = [aws_security_group.security_group_ssh.id]
+    subnet_id = aws_subnet.subnet_public_1a.id
+    vpc_security_group_ids = [
+      aws_security_group.security_group_ssh.id,
+      aws_security_group.security_group_exit_enable_all.id
+    ]
     associate_public_ip_address = true
         tags = {
         Name = "igor_dev${count.index}"
     }
 }
+
+resource "aws_instance" "igor_dev4" {
+  ami = "ami-07ebfd5b3428b6f4d" # Ubuntu Server 18.04 LTS
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  subnet_id = aws_subnet.subnet_public_1a.id
+  vpc_security_group_ids = [
+    aws_security_group.security_group_ssh.id,
+    aws_security_group.security_group_exit_enable_all.id
+  ]
+  associate_public_ip_address = true
+  tags = {
+    Name = "igor_dev4"
+  }
+  depends_on = [aws_s3_bucket.igor_dev4_s3]
+}
+
